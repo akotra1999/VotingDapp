65345e9a0a94e8551a00981375eacd5d1ecadcfe
unknown <akotra1999@gmail.com>
2021-06-17 16:55:23 -0700

    Add election display

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -9,13 +9,14 @@ contract Voting {
     mapping(bytes32 => uint) votes;
     bytes32[] candidates;
     bool open; // if true, anybody can register to vote
+    string title;
   }
 
   Election[] public elections;
   uint constant MAX_LENGTH = 15;
 
-  function createElection(address[] memory voterPool, bytes32[] memory candidates, bool open) public {
-    elections.push(Election({candidates: candidates, open: open}));
+  function createElection(address[] memory voterPool, bytes32[] memory candidates, bool open, string memory title) public {
+    elections.push(Election({candidates: candidates, open: open, title: title}));
 
     for(uint i = 0; i < voterPool.length; i++) {
       elections[elections.length - 1].eligibleVoters[voterPool[i]] = true;
@@ -39,10 +40,10 @@ contract Voting {
     elections[i].votes[candidate]++;
   }
 
-  function getElection(uint i) public view returns(bytes32[] memory, bool) {
+  function getElection(uint i) public view returns(bytes32[] memory, bool, string memory) {
     require(i >= 0, "Election not available");
     require(i < elections.length, "Election not available");
-    return (elections[i].candidates, elections[i].open);
+    return (elections[i].candidates, elections[i].open, elections[i].title);
   }
 
   function isEligibleToVote(uint i, address voter) public view returns (bool) {
@@ -59,6 +60,10 @@ contract Voting {
 
   function getNumberOfVotes(uint i, bytes32 candidate) public view returns (uint) {
     return elections[i].votes[candidate];
+  }
+
+  function getNumberOfElections() public view returns (uint) {
+    return elections.length;
   }
 
 }
--- src/app.js
+++ src/app.js
@@ -1,22 +1,69 @@
 App = {
 
-  search: async () => {
+  load: async () => {
 
-
-    // https://ethereum.org/en/developers/tutorials/set-up-web3js-to-use-ethereum-in-javascript/
-    // https://blockchain.oodles.io/dev-blog/interacting-with-ethereum-smart-contracts-through-web3js-library/
     if (window.ethereum) {
-      document.getElementById("candidates").innerHTML = "";
-      document.getElementById("open").innerHTML = "";
       window.web3 = new Web3(window.ethereum);
       let voting = await $.getJSON("Voting.json");
       let votingContract = new window.web3.eth.Contract(voting.abi, voting.networks["5777"].address);
-      let election = await votingContract.methods.getElection($("#search").val()).call();
-      document.getElementById("candidates").innerHTML = election[0].map(c => web3.utils.hexToAscii(c));
-      document.getElementById("open").innerHTML = election[1];
+      let numberOfElections = await votingContract.methods.getNumberOfElections().call();
+      let $electionTemplate = $("#election_template");
+
+      for (let i = numberOfElections - 1; i >= Math.max(0, numberOfElections - 3); i--) {
+        let $newElectionTemplate = $electionTemplate.clone();
+        let election = await votingContract.methods.getElection(i).call();
+        App.loadElectionCard($newElectionTemplate, election, i);
+        $("#recent_elections_list").append($newElectionTemplate);
+        $newElectionTemplate.show();
+      }
 
     } else {
       console.log("Can't find a Metamask account")
     }
+  },
+
+  search: async () => {
+
+    // https://ethereum.org/en/developers/tutorials/set-up-web3js-to-use-ethereum-in-javascript/
+    // https://blockchain.oodles.io/dev-blog/interacting-with-ethereum-smart-contracts-through-web3js-library/
+    if (window.ethereum) {
+      document.getElementById("search_result").style.display = "none";
+      window.web3 = new Web3(window.ethereum);
+      let voting = await $.getJSON("Voting.json");
+      let votingContract = new window.web3.eth.Contract(voting.abi, voting.networks["5777"].address);
+      let election = await votingContract.methods.getElection($("#search").val()).call();
+      let $searchResult = $("#search_result");
+      App.loadElectionCard($searchResult, election, $("#search").val());
+      $searchResult.show();
+
+    } else {
+      console.log("Can't find a Metamask account")
+    }
+  },
+
+  loadElectionCard: ($electionCard, electionData, electionID) => {
+    let candidates = electionData[0].map(c => web3.utils.hexToAscii(c));
+    let candidateString = "";
+
+    if (candidates.length == 1) {
+      candidateString = candidates[0];
+    } else if (candidates.length == 2) {
+      candidateString = candidates[0] + " and " + candidates[1];
+    } else if (candidates.length != 0) {
+      candidateString = candidates[0] + ", " + candidates[1] + ", etc.";
+    }
+    $electionCard.find("#candidates").html(candidateString);
+    $electionCard.find("#election_id").html("#" + electionID);
+
+    if (electionData[1]) {
+      $electionCard.find("#open").html("Anybody can vote!");
+    } else {
+      $electionCard.find("#open").html("Click on register to check eligiblity.");
+    }
+    $electionCard.find("#title").html(electionData[2]);
   }
 }
+
+$(window).load(() => {
+  App.load();
+});
